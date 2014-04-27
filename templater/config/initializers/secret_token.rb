54ac2d3bcfe2466b5063817c432cbefc04efdc39
Bruce P <bruce@binarywebpark.com>
2014-04-26 23:21:32 -0700

    chapter 3 templater implementation

---
+++ templater/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Templater::Application.config.secret_key_base = 'e1bee99378bd9c9bba43c43b7be77bca8bc4ab0212e89b681716261c58f4ea3489de01bce38a2978c99749ead03719d904e1a295797d4d9dc928eae4942d1c31'
---
+++ templater/test/test_helper.rb
@@ -0,0 +1,24 @@
+ENV["RAILS_ENV"] = "test"
+require File.expand_path('../../config/environment', __FILE__)
+require 'rails/test_help'
+
+require "capybara"
+require "capybara/rails"
+
+# Define a bare test case to use with Capybara
+class ActiveSupport::IntegrationCase < ActiveSupport::TestCase
+  include Capybara::DSL
+  include Rails.application.routes.url_helpers
+end
+
+class ActiveSupport::TestCase
+  ActiveRecord::Migration.check_pending!
+
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  #
+  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # -- they do not yet inherit this setting
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+end
